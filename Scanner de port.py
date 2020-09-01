781e78582da2998f16fc2714e295c8e0f90d37c2
GaetanAc <49526032+GaetanAc@users.noreply.github.com>
2020-09-01 08:42:51 +0200

    Add files via upload

---
+++ Scanner de port.py
@@ -0,0 +1,16 @@
+import socket
+
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.settimeout(10)
+
+host = input( "Entrer l'adresse IP a scan :")
+port = int(input("Entrer le numéro du port a scan: "))
+
+def PortScanner(port):
+    if s.connect_ex((host,port)):
+        print(" Le port est fermé")
+    else :
+        print("Le port est ouvert")
+
+PortScanner(port)
+
